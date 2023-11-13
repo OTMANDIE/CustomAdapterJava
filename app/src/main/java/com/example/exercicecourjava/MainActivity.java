d6c5ba5f84b330d0ea166bb1923d8a02892dff07
OTMANDIE <afaneotmane2006@gmail.com>
2023-11-13 17:42:09 +0100

    first commit

---
+++ app/src/main/java/com/example/exercicecourjava/MainActivity.java
@@ -0,0 +1,51 @@
+    package com.example.exercicecourjava;
+
+    import androidx.appcompat.app.AppCompatActivity;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.ArrayAdapter;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.ListView;
+    import java.util.ArrayList;
+    import java.util.List;
+
+    public class MainActivity extends AppCompatActivity {
+
+        ListView lv;
+        List<String> countries = new ArrayList<String>();
+        MyAdapter adapter;
+        EditText editText;
+        Button addButton;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            lv = findViewById(R.id.CountriesList);
+            editText = findViewById(R.id.editText);
+            addButton = findViewById(R.id.addButton);
+
+            countries.add("US");
+            countries.add("UK");
+            countries.add("Morocco");
+            countries.add("Tunisia");
+            countries.add("Lebanon");
+
+            adapter = new MyAdapter(this, countries);
+            lv.setAdapter(adapter);
+
+            addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String newCountry = editText.getText().toString();
+                    if (!newCountry.isEmpty()) {
+                        countries.add(newCountry);
+                        lv.setAdapter(adapter);
+                        editText.setText("");
+                    }
+                }
+            });
+        }
+    }
---
+++ app/src/main/java/com/example/exercicecourjava/MyAdapter.java
@@ -0,0 +1,46 @@
+package com.example.exercicecourjava;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends BaseAdapter {
+    List<String> data;
+    Context context;
+    MyAdapter(Context context,List<String> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1,null);
+            TextView text = view.findViewById(android.R.id.text1);
+            text.setText(data.get(position).toString());
+        }
+        return view;
+    }
+}
